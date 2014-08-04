19cca0b1bea6eac761fad764ea5bfa971ce74026
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-08-04 13:03:58 -0400

    Add basic render class

---
+++ model/obj.js
@@ -0,0 +1,21 @@
+/**
+ *	@file
+ *	Core model logic.
+ */
+
+/**
+ *	The render object.
+ */
+function benderRender(renderArray)	{
+
+	/**
+	 *	@var array renderArray
+	 *		The render array to work with
+	 */
+	this.renderArray;
+
+	 /**
+	  *	Construction logic.
+	  */
+	this.renderArray = renderArray;
+}
